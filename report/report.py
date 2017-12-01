2e6ee876bd67c8642a11c3ee05d358261cbc4c72
Alvaro del Castillo <acs@bitergia.com>
2017-12-01 03:09:26 +0100

    Fix pending PEP-8 issues

--- report/report.py
+++ report/report.py
@@ -35,13 +35,12 @@ import prettyplotlib as ppl
 import numpy as np
 
 from collections import OrderedDict
-from datetime import date, datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from distutils.dir_util import copy_tree
 
 from dateutil import parser, relativedelta
 
 from .metrics import git
-from .metrics import its
 from .metrics import jira
 from .metrics import github_issues
 from .metrics import github_prs
@@ -52,6 +51,7 @@ from .metrics import stackexchange
 from .metrics.metrics import Metrics
 
 logger = logging.getLogger(__name__)
+
 
 class Report():
     GIT_INDEX = 'git_enrich'
@@ -153,8 +153,8 @@ class Report():
                 else:
                     for metric_section in ds_config[section]:
                         if ds_config[section][metric_section] is not None:
-                            if metric_section not in new_config[section] or \
-                                new_config[section][metric_section] is None:
+                            if (metric_section not in new_config[section] or
+                                new_config[section][metric_section] is None):
                                 new_config[section][metric_section] = ds_config[section][metric_section]
                             else:
                                 new_config[section][metric_section] += ds_config[section][metric_section]
@@ -169,7 +169,7 @@ class Report():
             ds = data_sources[i]
             ds_config = self.ds2class[ds].get_section_metrics()
             activity_metrics = ds_config['project_activity']['metrics']
-            new_config['project_activity']['ds' + str(i+1)+"_metrics"] = activity_metrics
+            new_config['project_activity']['ds' + str(i + 1) + "_metrics"] = activity_metrics
 
         # from pprint import pprint
         # pprint(new_config)
@@ -202,17 +202,16 @@ class Report():
         plt.title(title)
         y_pos = np.arange(len(data1))
 
-        ppl.bar(xpos+width+width, data3, color="orange", width=0.28, annotate=True)
-        ppl.bar(xpos+width, data1, color='grey', width=0.28, annotate=True)
-        ppl.bar(xpos, data2, grid='y', width = 0.28, annotate=True)
-        plt.xticks(xpos+width, labels)
+        ppl.bar(xpos + width + width, data3, color="orange", width=0.28, annotate=True)
+        ppl.bar(xpos + width, data1, color='grey', width=0.28, annotate=True)
+        ppl.bar(xpos, data2, grid='y', width=0.28, annotate=True)
+        plt.xticks(xpos + width, labels)
         plt.legend(legend, loc=2)
-
 
         plt.savefig(file_name)
         plt.close()
 
-    def bar_chart(self, title, labels, data1, file_name, data2 = None, legend=["", ""]):
+    def bar_chart(self, title, labels, data1, file_name, data2=None, legend=["", ""]):
 
         colors = ["orange", "grey"]
 
@@ -227,14 +226,14 @@ class Report():
         y_pos = np.arange(len(data1))
 
         if data2 is not None:
-            ppl.bar(xpos+width, data1, color="orange", width=0.35, annotate=True)
-            ppl.bar(xpos, data2, grid='y', width = 0.35, annotate=True)
-            plt.xticks(xpos+width, labels)
+            ppl.bar(xpos + width, data1, color="orange", width=0.35, annotate=True)
+            ppl.bar(xpos, data2, grid='y', width=0.35, annotate=True)
+            plt.xticks(xpos + width, labels)
             plt.legend(legend, loc=2)
 
         else:
             ppl.bar(xpos, data1, grid='y', annotate=True)
-            plt.xticks(xpos+width, labels)
+            plt.xticks(xpos + width, labels)
 
         plt.savefig(file_name)
         plt.close()
@@ -278,36 +277,37 @@ class Report():
         """
 
         author = self.config['overview']['author_metrics'][0]
-        csv_labels = 'labels,'+author.id
+        csv_labels = 'labels,' + author.id
         file_label = author.ds.name + "_" + author.id
-        title_label = author.name + " per "+ self.interval
+        title_label = author.name + " per " + self.interval
         self.__create_csv_eps(author, None, csv_labels, file_label, title_label)
 
         logger.debug("CSV file %s generation in progress", file_name)
 
         bmi = []
-        ttc = [] # time to close
+        ttc = []  # time to close
 
         csv_labels = ''
         for m in self.config['overview']['bmi_metrics']:
             metric = m(self.es_url, self.get_metric_index(m),
                        start=self.end_prev_month, end=self.end)
-            csv_labels += m.id +","
+            csv_labels += m.id + ","
             bmi.append(metric.get_agg())
 
         for m in self.config['overview']['time_to_close_metrics']:
             metric = m(self.es_url, self.get_metric_index(m),
                        start=self.end_prev_month, end=self.end)
-            csv_labels += m.id +","
+            csv_labels += m.id + ","
             ttc.append(metric.get_agg())
 
-        csv = csv_labels[:-1]+"\n"  # remove last comma
-        csv = csv.replace("_","")
+        csv = csv_labels[:-1] + "\n"  # remove last comma
+        csv = csv.replace("_", "")
         for val in bmi:
             csv += "%s," % (self.str_val(val))
         for val in ttc:
             csv += "%s," % (self.str_val(val))
-        if csv[-1] == ',': csv = csv [:-1]
+        if csv[-1] == ',':
+            csv = csv[:-1]
         file_name = os.path.join(self.data_dir, 'efficiency.csv')
         with open(file_name, "w") as f:
             f.write(csv)
@@ -315,12 +315,12 @@ class Report():
         logger.debug("CSV file: %s was generated", file_name)
 
     def sec_com_channels(self):
-
-        for metric in self.config['com_channels']['activity_metrics'] + \
-                      self.config['com_channels']['author_metrics']:
-            csv_labels = 'labels,'+metric.id
+        metrics = self.config['com_channels']['activity_metrics']
+        metrics += self.config['com_channels']['author_metrics']
+        for metric in metrics:
+            csv_labels = 'labels,' + metric.id
             file_label = metric.ds.name + "_" + metric.id
-            title_label = metric.name + " per "+ self.interval
+            title_label = metric.name + " per " + self.interval
             self.__create_csv_eps(metric, None, csv_labels, file_label, title_label)
 
     @classmethod
@@ -341,10 +341,10 @@ class Report():
         logger.debug("CSV file %s generation in progress", file_label)
 
         esfilters = None
-        csv_labels = csv_labels.replace("_","")  # LaTeX not supports
+        csv_labels = csv_labels.replace("_", "")  # LaTeX not supports
 
         if project and project != self.GLOBAL_PROJECT:
-            esfilters={"project": project}
+            esfilters = {"project": project}
         m1 = metric1(self.es_url, self.get_metric_index(metric1),
                      esfilters=esfilters,
                      start=self.start, end=self.end)
@@ -366,19 +366,19 @@ class Report():
             csv += "\n"
 
         if project:
-            file_name = os.path.join(self.data_dir, file_label+"_"+project+".csv")
+            file_name = os.path.join(self.data_dir, file_label + "_" + project + ".csv")
         else:
-            file_name = os.path.join(self.data_dir, file_label+".csv")
+            file_name = os.path.join(self.data_dir, file_label + ".csv")
         with open(file_name, "w") as f:
             f.write(csv)
 
         logger.debug("CSV file %s was generated", file_label)
 
         if project:
-            file_name = os.path.join(self.data_dir, file_label+"_"+project+".eps")
-            title = title_label+": "+ project
+            file_name = os.path.join(self.data_dir, file_label + "_" + project + ".eps")
+            title = title_label + ": " + project
         else:
-            file_name = os.path.join(self.data_dir, file_label+".eps")
+            file_name = os.path.join(self.data_dir, file_label + ".eps")
             title = title_label
         x_val = [parser.parse(val).strftime("%y-%m") for val in m1_ts['date']]
         if metric2:
@@ -395,10 +395,10 @@ class Report():
         """
 
         def create_data(metrics, project):
-            csv_labels = "labels" + ',' + metrics[0].id + ","+metrics[1].id
+            csv_labels = "labels" + ',' + metrics[0].id + "," + metrics[1].id
             file_label = metrics[0].ds.name + "_" + metrics[0].id + "_"
             file_label += metrics[1].ds.name + "_" + metrics[1].id
-            title_label = metrics[0].name+", "+ metrics[1].name + " per "+ self.interval
+            title_label = metrics[0].name + ", " + metrics[1].name + " per " + self.interval
             self.__create_csv_eps(metrics[0], metrics[1], csv_labels,
                                   file_label, title_label, project)
 
@@ -410,16 +410,15 @@ class Report():
             metrics = self.config['project_activity'][activity_ds]
             create_data(metrics, project)
 
-
     def sec_project_community(self, project=None):
 
         def create_csv(metric1, csv_labels, file_label):
             esfilters = None
-            csv_labels = csv_labels.replace("_","")  # LaTeX not supports "_"
+            csv_labels = csv_labels.replace("_", "")  # LaTeX not supports "_"
             if project != self.GLOBAL_PROJECT:
-                esfilters={"project": project}
+                esfilters = {"project": project}
 
-            file_name = os.path.join(self.data_dir, file_label+"_"+project+".csv")
+            file_name = os.path.join(self.data_dir, file_label + "_" + project + ".csv")
 
             logger.debug("CSV file %s generation in progress", file_name)
 
@@ -438,13 +437,12 @@ class Report():
 
             logger.debug("CSV file %s was generated", file_name)
 
-
         logger.info("Community data for: %s", project)
 
         author = self.config['project_community']['author_metrics'][0]
-        csv_labels = 'labels,'+author.id
+        csv_labels = 'labels,' + author.id
         file_label = author.ds.name + "_" + author.id
-        title_label = author.name + " per "+ self.interval
+        title_label = author.name + " per " + self.interval
         self.__create_csv_eps(author, None, csv_labels, file_label, title_label,
                               project)
 
@@ -454,8 +452,8 @@ class Report():
         """
         metric = self.config['project_community']['people_top_metrics'][0]
         # TODO: Commits must be extracted from metric
-        csv_labels = author.id+",commits"
-        file_label = author.ds.name+"_top_"+ author.id
+        csv_labels = author.id + ",commits"
+        file_label = author.ds.name + "_top_" + author.id
         create_csv(metric, csv_labels, file_label)
 
         """
@@ -464,8 +462,8 @@ class Report():
         """
         orgs = self.config['project_community']['orgs_top_metrics'][0]
         # TODO: Commits must be extracted from metric
-        csv_labels = orgs.id+",commits"
-        file_label = orgs.ds.name+"_top_"+ orgs.id
+        csv_labels = orgs.id + ",commits"
+        file_label = orgs.ds.name + "_top_" + orgs.id
         create_csv(orgs, csv_labels, file_label)
 
     def sec_project_process(self, project=None):
@@ -479,7 +477,7 @@ class Report():
         """
         for i in range(0, len(self.config['project_process']['bmi_metrics'])):
             metric = self.config['project_process']['bmi_metrics'][i]
-            csv_labels = "labels"+","+metric.id
+            csv_labels = "labels" + "," + metric.id
             file_label = metric.ds.name + "_" + metric.id
             title_label = metric.name
             self.__create_csv_eps(metric, None, csv_labels, file_label, title_label,
@@ -493,7 +491,7 @@ class Report():
 
         if len(self.config['project_process']['time_to_close_metrics']) > 0:
             metrics = self.config['project_process']['time_to_close_metrics']
-            csv_labels = "labels" + ',' + metrics[0].id + ","+metrics[1].id
+            csv_labels = "labels" + ',' + metrics[0].id + "," + metrics[1].id
             file_label = metrics[0].ds.name + "_" + metrics[0].id + "_"
             file_label += metrics[1].ds.name + "_" + metrics[1].id
             # title_label = metrics[0].name + ", " + metrics[1].name + " per "+ self.interval
@@ -512,12 +510,12 @@ class Report():
             while i < len(metrics):
                 # Generate the metrics in data source pairs.
                 # TODO: convert to tuples metrics
-                csv_labels = "labels" + ',' + metrics[i].id + ","+metrics[i+1].id
+                csv_labels = "labels" + ',' + metrics[i].id + "," + metrics[i + 1].id
                 file_label = metrics[i].ds.name + "_" + metrics[i].id + "_"
-                file_label += metrics[i+1].ds.name + "_" + metrics[i+1].id
+                file_label += metrics[i + 1].ds.name + "_" + metrics[i + 1].id
                 # title_label = metrics[0].name+", "+ metrics[1].name + " per "+ self.interval
                 title_label = self.config['project_process']['time_to_close_review_title']
-                self.__create_csv_eps(metrics[i], metrics[i+1], csv_labels, file_label,
+                self.__create_csv_eps(metrics[i], metrics[i + 1], csv_labels, file_label,
                                       title_label, project)
                 i = i + 2
 
@@ -528,7 +526,7 @@ class Report():
         """
         if self.config['project_process']['patchsets_metrics']:
             metrics = self.config['project_process']['patchsets_metrics']
-            csv_labels = "labels" + ',' + metrics[0].id + ","+metrics[1].id
+            csv_labels = "labels" + ',' + metrics[0].id + "," + metrics[1].id
             file_label = metrics[0].ds.name + "_" + metrics[0].id + "_"
             file_label += metrics[1].ds.name + "_" + metrics[1].id
             # title_label = metrics[0].name+", "+ metrics[1].name + " per "+ self.interval
@@ -591,7 +589,7 @@ class Report():
     def create_data_figs(self):
         """ Generate the data and figs files for the report """
         logger.info("Generating the report data and figs from %s to %s",
-                     self.start, self.end)
+                    self.start, self.end)
 
         for section in self.sections():
             logger.info("Generating %s", section)
@@ -612,7 +610,7 @@ class Report():
             name = pdate.strftime('%Y')
             # The month is the next month 2017-04-01 to the current quarter
             month = int(pdate.strftime('%m')) - 1
-            quarter = int(round(int(month)/3, 0))
+            quarter = int(round(int(month) / 3, 0))
             name += "-Q" + str(quarter)
 
         return name
@@ -623,7 +621,7 @@ class Report():
         # First step is to create the report dir from the template
         report_path = self.data_dir
         templates_path = os.path.join(os.path.dirname(__file__),
-                                        "latex_template")
+                                      "latex_template")
         copy_tree(templates_path, report_path)
         # Copy the data generated to be used in LaTeX template
         copy_tree(self.data_dir, os.path.join(report_path, "data"))
@@ -635,15 +633,15 @@ class Report():
         # Change the quarter subtitle
         period_name = self.period_name(self.end, self.interval, self.offset)
         period_replace = period_name.replace(' ', r'\ ')
-        cmd = ['grep -rl 2016-QUARTER . | xargs sed -i s/2016-QUARTER/' + period_replace +  '/g']
+        cmd = ['grep -rl 2016-QUARTER . | xargs sed -i s/2016-QUARTER/' + period_replace + '/g']
         # cmd = ['sed -i s/2016-QUARTER/' + self.end.strftime('%Y-%m-%d') + \
         #       r'\ ' + self.interval + '/g *.tex']
         subprocess.call(cmd, shell=True, cwd=report_path)
         # Report date frame
         quarter_start = self.end - relativedelta.relativedelta(months=3)
         quarter_start += relativedelta.relativedelta(days=1)
-        dateframe = (quarter_start.strftime('%Y-%m-%d')+" to "+self.end.strftime('%Y-%m-%d')).replace(' ', r'\ ')
-        cmd = ['grep -rl DATEFRAME . | xargs sed -i s/DATEFRAME/' + dateframe +  '/g']
+        dateframe = (quarter_start.strftime('%Y-%m-%d') + " to " + self.end.strftime('%Y-%m-%d')).replace(' ', r'\ ')
+        cmd = ['grep -rl DATEFRAME . | xargs sed -i s/DATEFRAME/' + dateframe + '/g']
         subprocess.call(cmd, shell=True, cwd=report_path)
         # Change the date Copyright
         cmd = [r'sed -i s/\(cc\)\ 2016/\(cc\)\ ' + datetime.now().strftime('%Y') + '/g *.tex']
@@ -654,12 +652,11 @@ class Report():
         cmd = [r'sed -i "s/^#//g" data/git_top_organizations_*']
         subprocess.call(cmd, shell=True, cwd=report_path)
 
-
         # Activity section
         activity = ''
         for activity_ds in ['git', 'github', 'gerrit', 'mls']:
             if activity_ds in self.data_sources:
-                activity += r"\input{activity/" +  activity_ds + ".tex}"
+                activity += r"\input{activity/" + activity_ds + ".tex}"
 
         with open(os.path.join(report_path, "activity.tex"), "w") as flatex:
             flatex.write(activity)
@@ -668,7 +665,7 @@ class Report():
         community = ''
         for community_ds in ['git', 'mls']:
             if community_ds in self.data_sources:
-                community += r"\input{community/" +  community_ds + ".tex}"
+                community += r"\input{community/" + community_ds + ".tex}"
 
         with open(os.path.join(report_path, "community.tex"), "w") as flatex:
             flatex.write(community)
@@ -677,7 +674,7 @@ class Report():
         overview = r'\input{overview/summary.tex}'
         for overview_ds in ['github', 'gerrit']:
             if overview_ds in self.data_sources:
-                overview += r"\input{overview/efficiency-" +  overview_ds + ".tex}"
+                overview += r"\input{overview/efficiency-" + overview_ds + ".tex}"
 
         with open(os.path.join(report_path, "overview.tex"), "w") as flatex:
             flatex.write(overview)
@@ -686,7 +683,7 @@ class Report():
         process = ''
         for process_ds in ['github_prs', 'gerrit']:
             if process_ds in self.data_sources:
-                process += r"\input{process/" +  process_ds + ".tex}"
+                process += r"\input{process/" + process_ds + ".tex}"
 
         with open(os.path.join(report_path, "process.tex"), "w") as flatex:
             flatex.write(process)
